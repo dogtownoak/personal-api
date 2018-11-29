24c0b05de3a34755ca6a74f42b31705b21c2c64a
dogtownoak <david.brandon.ford@gmail.com>
2018-11-29 11:15:41 -0800

    added Procfile

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ const
     express = require('express'),
     app = express(),
     bodyParser = require('body-parser')
+    db = require('./models')
 
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(express.json())
 
 app.use(express.static)
+
+
+
+
+app.listen(process.env.PORT || 3000)
