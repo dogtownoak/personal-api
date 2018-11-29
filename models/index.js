eea85f53ff1c284de171e7278e05541782f2aa8b
dogtownoak <david.brandon.ford@gmail.com>
2018-11-29 12:06:45 -0800

    updated file structure

--- models/index.js
+++ models/index.js
@@ -1,2 +1,2 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/personal-api")
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/personal-api", { useNewUrlParser: true });
