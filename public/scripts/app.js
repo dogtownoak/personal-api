a651be70d92fbf7a77760ef8091357fe94b9eda1
dogtownoak <david.brandon.ford@gmail.com>
2018-12-01 14:42:27 -0800

    updated form

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -6,6 +6,10 @@ $(document).ready(function(){
 
 var url = "http://localhost:3000/api/art"
 
+$('form').on( 'submit', function( e ) {
+    e.preventDefault()
+    console.log("click")
+  });
 
 $.ajax({
     method: 'GET',
@@ -58,4 +62,33 @@ $.ajax({
 });
 
 
+// var date = $('input[type="date"]').val();
+// var dateKey = `date=${date}&`;
+// var startDate = `start_date=${date}&`;
+// var endDate = "end_date=2018-11-19&";
 
+
+/////////// FORM CREATE ART ///////////////////////////
+// $('#create').click(function(e){
+//     e.preventDefault()
+//     $.ajax({
+//         method: 'POST',
+//         url: `${url}`,
+//         data: $("#form").serialize()
+//         ,
+//         success: function( response ) {
+//             console.log( response ); 
+//             console.log ($('#img[type="text]').val())  
+//             console.log( $("#form").serialize())     
+     
+//         },
+//         error: function() {
+//             console.log('There was an error creating art');
+//         }
+//     })
+// });
+
+
+// $( 'form').on( "submit", function( event ) {
+//     console.log( $( this ).serialize().val() );
+//   });
